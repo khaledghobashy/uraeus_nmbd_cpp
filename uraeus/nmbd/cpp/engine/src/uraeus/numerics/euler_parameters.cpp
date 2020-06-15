529a2e72c0263589a3d4cb9c74d118c4c0a81f53
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-06-15 21:39:40 +0200

    minor edits in A and B matricies

--- uraeus/nmbd/cpp/engine/src/uraeus/numerics/euler_parameters.cpp
+++ uraeus/nmbd/cpp/engine/src/uraeus/numerics/euler_parameters.cpp
@@ -94,17 +94,18 @@ Eigen::Matrix3d A(const Eigen::Vector4d& P)
 
     Eigen::Matrix3d A;
 
-    A(0,0) = pow(e0,2) + pow(e1,2) - pow(e2,2) - pow(e3,2);
-    A(0,1) = 2*((e1*e2) - (e0*e3));
-    A(0,2) = 2*((e1*e3) + (e0*e2));
-    
-    A(1,0) = 2*((e1*e2) + (e0*e3));
-    A(1,1) = pow(e0,2) - pow(e1,2) + pow(e2,2) - pow(e3,2);
-    A(1,2) = 2*((e2*e3) - (e0*e1));
-    
-    A(2,0) = 2*((e1*e3) - (e0*e2));
-    A(2,1) = 2*((e2*e3) + (e0*e1));
-    A(2,2) = pow(e0,2) - pow(e1,2) - pow(e2,2) + pow(e3,2);
+    A <<
+    (e0*e0) + (e1*e1) - (e2*e2) - (e3*e3), 
+    2*((e1*e2) - (e0*e3)), 
+    2*((e1*e3) + (e0*e2)),
+
+    2*((e1*e2) + (e0*e3)), 
+    (e0*e0) - (e1*e1) + (e2*e2) - (e3*e3),
+    2*((e2*e3) - (e0*e1)),
+
+    2*((e1*e3) - (e0*e2)),
+    2*((e2*e3) + (e0*e1)),
+    (e0*e0) - (e1*e1) - (e2*e2) + (e3*e3);
 
     return A;
 };
@@ -123,20 +124,21 @@ Eigen::Matrix<double, 3, 4> B(const Eigen::Vector4d& P, const Eigen::Vector3d& u
     const double& uy = u[1];
     const double& uz = u[2];
     
-    mat(0,0) = 2*e0*ux + 2*e2*uz - 2*e3*uy ;
-    mat(0,1) = 2*e1*ux + 2*e2*uy + 2*e3*uz ;
-    mat(0,2) = 2*e0*uz + 2*e1*uy - 2*e2*ux ;
-    mat(0,3) = -2*e0*uy + 2*e1*uz - 2*e3*ux ;
+    mat << 
+     2*e0*ux + 2*e2*uz - 2*e3*uy , 
+     2*e1*ux + 2*e2*uy + 2*e3*uz , 
+     2*e0*uz + 2*e1*uy - 2*e2*ux , 
+    -2*e0*uy + 2*e1*uz - 2*e3*ux ,
     
-    mat(1,0) = 2*e0*uy - 2*e1*uz + 2*e3*ux ;
-    mat(1,1) = -2*e0*uz - 2*e1*uy + 2*e2*ux ;
-    mat(1,2) = 2*e1*ux + 2*e2*uy + 2*e3*uz ;
-    mat(1,3) = 2*e0*ux + 2*e2*uz - 2*e3*uy ;
+     2*e0*uy - 2*e1*uz + 2*e3*ux ,
+    -2*e0*uz - 2*e1*uy + 2*e2*ux ,
+     2*e1*ux + 2*e2*uy + 2*e3*uz ,
+     2*e0*ux + 2*e2*uz - 2*e3*uy ,
     
-    mat(2,0) = 2*e0*uz + 2*e1*uy - 2*e2*ux ;
-    mat(2,1) = 2*e0*uy - 2*e1*uz + 2*e3*ux ;
-    mat(2,2) = -2*e0*ux - 2*e2*uz + 2*e3*uy ;
-    mat(2,3) = 2*e1*ux + 2*e2*uy + 2*e3*uz ;
+     2*e0*uz + 2*e1*uy - 2*e2*ux ,
+     2*e0*uy - 2*e1*uz + 2*e3*ux ,
+    -2*e0*ux - 2*e2*uz + 2*e3*uy ,
+     2*e1*ux + 2*e2*uy + 2*e3*uz ;
 
     return mat;
 
